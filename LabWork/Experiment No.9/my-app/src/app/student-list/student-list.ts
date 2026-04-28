cfbacd9b6c85b76be601ff9a6280b9f2e4b21029
PradipP07 <patilpradeep5055@gmail.com>
2026-04-28 23:08:50 +0530

    WT Exp

--- LabWork/Experiment No.9/my-app/src/app/student-list/student-list.ts
+++ LabWork/Experiment No.9/my-app/src/app/student-list/student-list.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-student-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './student-list.html',
   styleUrl: './student-list.css',
 })
